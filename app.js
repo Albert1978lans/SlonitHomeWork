2c158e9b16ffc170e1234fc514d4927a8b34fef1
Albert <bert21387@gmail.com>
2025-02-11 21:21:20 +0500

    renamed the gitignore file and created the file app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hello, from console")
+
+// C:\Users\ya>cd /d d:\slonit-turorial\HomeWork
+// d:\slonit-turorial\HomeWork>node app.js
+
+// Короче правильно так:
+// C:\Users\Karen>cd C:\Users\Karen\Desktop\prostoReact
+// Или:
+// C:\Users\Karen>cd Desktop\prostoReact
+// Если другой диск так:
+// C:\Users\Karen>cd /d z:\prostoReact
+
+// Всё, ты в папке, а дальше уже:
+// C:\Users\Karen\Desktop\prostoReact>npm install
+// И тд, что там тебе по ноде требуется...
